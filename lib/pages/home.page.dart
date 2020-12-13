e5d8dc4492a8d4910c992d19f1e2495e521af832
Felipe Minello <felipeminello@gmail.com>
2020-12-12 21:07:40 -0300

    product list part 1

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -14,6 +14,40 @@ class HomePage extends StatelessWidget {
               height: 60,
             ),
             search(),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "Categories",
+              style: TextStyle(fontSize: 30),
+            ),
+            Container(
+              height: 90,
+              child: categoryList(),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Best Selling',
+                  style: TextStyle(fontSize: 30),
+                ),
+                FlatButton(
+                  child: Text('SeeAll'),
+                  onPressed: () => {},
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 350,
+              child: productList(),
+            ),
           ],
         ),
       ),
@@ -44,7 +78,8 @@ Widget search() {
           child: TextFormField(
             keyboardType: TextInputType.text,
             decoration: InputDecoration(
-              labelText: 'Search...',
+              border: InputBorder.none,
+              hintText: 'Search...',
               labelStyle: TextStyle(
                 color: Colors.black,
                 fontWeight: FontWeight.w400,
@@ -58,5 +93,68 @@ Widget search() {
         ),
       ],
     ),
+  );
+}
+
+Widget categoryList() {
+  return Container(
+    child: ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+        categoryItem(),
+      ],
+    ),
+  );
+}
+
+Widget categoryItem() {
+  return Container(
+    width: 70,
+    height: 70,
+    margin: EdgeInsets.all(10),
+    padding: EdgeInsets.all(10),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      boxShadow: [
+        new BoxShadow(
+          color: Colors.black12,
+          offset: new Offset(1, 1),
+          blurRadius: 5,
+          spreadRadius: 5,
+        ),
+      ],
+      borderRadius: BorderRadius.all(
+        Radius.circular(64),
+      ),
+    ),
+    child: Image.asset("assets/Icon_Devices.png"),
+  );
+}
+
+Widget productList() {
+  return Container(
+    child: ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        productItem(),
+        productItem(),
+      ],
+    ),
+  );
+}
+
+Widget productItem() {
+  return Container(
+    padding: EdgeInsets.all(10),
+    margin: EdgeInsets.all(5),
+    width: 170,
+    color: Colors.black12,
+    child: Text('Produto'),
   );
 }
